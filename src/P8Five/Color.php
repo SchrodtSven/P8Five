09daf369ea7a278f1ec1414cdd4f3b73cfaea1fd
Sven Schrodt <sven@schrodt.club>
2025-11-24 14:20:58 +0100

    Re-writing tests with dataProvider &/|| testWith

--- src/P8Five/Color.php
+++ src/P8Five/Color.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
- * Class for demonstrating "clone with" funtionality on readonly classes
+ * Class for demonstrating "clone with" funtionality on readonly classes in unit tests
  * 
  * @author Sven Schrodt<sven@schrodt.nrw>
  * @link https://github.com/SchrodtSven/P8Five
@@ -16,11 +16,13 @@ namespace SchrodtSven\P8Five;
 
 readonly class Color
 {
+    public const int ALPHA = 255;
+
     public function __construct(
         public int $red,
         public int $green,
         public int $blue,
-        public int $alpha = 255,
+        public int $alpha = self::ALPHA,
     ) {}
 
     public function withAlpha(int $alpha): self
--- test/CloneWithTest.php
+++ test/CloneWithTest.php
@@ -12,31 +12,29 @@ declare(strict_types=1);
  */
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestWith;
+
 use SchrodtSven\P8Five\Color;
 
 final class CloneWithTest  extends TestCase
 {
-    private Color $col;
-
-    public function setUp(): void
+    
+    #[TestWith([100, 22, 122, 23])]
+    #[TestWith([254, 222, 102, 23])]
+    public function testBasix(int $r, int $g, int $b, int $a): void
     {
-        $this->col = new Color(23, 42, 5);
-    }
+        $tmp = new Color($r ,$g, $b);
+        $this->assertSame(Color::ALPHA, $tmp->alpha);
+        $this->assertSame($r, $tmp->red);
+        $this->assertSame($g, $tmp->green);
+        $this->assertSame($b, $tmp->blue);
 
-    public function testBasix(): void
-    {
+        $tmp2 = $tmp->withAlpha($a);
+        $this->assertSame($a, $tmp2->alpha);
+        $this->assertSame($r, $tmp2->red);
+        $this->assertSame($g, $tmp2->green);
+        $this->assertSame($b, $tmp2->blue);
         
-        $tmp = $this->col->withAlpha(99);
-        
-        $this->assertSame(99, $tmp->alpha);
     }
 
-
-    public static function txtProvider(): array
-    {
-        return [
-            'data set 1' => [' Lorem Ispum Foo ! ', 'lorem-ispum-foo-!'],
-            
-        ];
-    }
 }
